fd05f9a417c8eee771fe559b19cdc89690075965
rmantay <rodrigo.mantay@mercadolibre.com>
2021-03-04 13:30:06 -0300

    Realizado el ejercicio 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,17 +8,41 @@ public class Main {
 
     public static void main(String[] args)
     {
-        printer.print("Ingrese el numero hasta el que desee calcular los numeros primos ");
+        Scanner scanner = new Scanner(System.in);
+        if (args.length > 0) {
+            printer.print("Ingrese el numero hasta el que desee calcular los numeros primos ");
+            int len = scanner.nextInt();
 
-        Scanner scanner= new Scanner(System.in);
-        int len=scanner.nextInt();
-
-        HashMap<Integer, Boolean> nums = new HashMap<Integer, Boolean>();
-            for (int i=0; i <= len; i++)
-            {
+            HashMap<Integer, Boolean> nums = new HashMap<Integer, Boolean>();
+            for (int i = 0; i <= len; i++) {
                 nums.put(i, true);
             }
             sieve.getPrimes(nums);
+        } else {
+            printer.print("Ingrese la cantidad de numeros a mostrar ");
+            int n = scanner.nextInt();
+            printer.print("Ingrese la cantidad de repeticiones");
+            int m= scanner.nextInt();
+            printer.print("Ingrese el numero que quiere evaluar la repeticion");
+            String sTextoBuscado = scanner.next();
+
+            int start_at = -1 + (int)Math.pow(10, String.valueOf(m).length() - 1);
+            int nprinted = 0;
+            while (nprinted < n){
+                String sTexto = String.valueOf(start_at);
+                int cantidad_de_chars_matcheados = 0;
+                while (sTexto.indexOf(sTextoBuscado) > -1) {
+                    sTexto = sTexto.substring(sTexto.indexOf(sTextoBuscado)+sTextoBuscado.length(),sTexto.length());
+                    cantidad_de_chars_matcheados++;
+                }
+                if (cantidad_de_chars_matcheados == m) {
+                    nprinted++;
+                    printer.print(String.valueOf(start_at));
+                }
+                start_at++;
+            }
+
+        }
     }
 
 }
