5f698593877bd8d214184d82f6a31467110a828e
rmantay <rodrigo.mantay@mercadolibre.com>
2021-03-04 12:47:19 -0300

    Se agrego el Scanner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,23 +2,23 @@ package com.company;
 import classes.*;
 import java.lang.reflect.Array;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        if (args.length > 1){
-            int len = Integer.parseInt(args[1]);
-            HashMap<Integer, Boolean> nums = new HashMap<Integer, Boolean>();
-            for (int i=1; i <= len; i++){
+    public static void main(String[] args)
+    {
+        printer.print("Ingrese el numero hasta el que desee calcular los numeros primos ");
+
+        Scanner scanner= new Scanner(System.in);
+        int len=scanner.nextInt();
+
+        HashMap<Integer, Boolean> nums = new HashMap<Integer, Boolean>();
+            for (int i=0; i <= len; i++)
+            {
                 nums.put(i, true);
             }
             sieve.getPrimes(nums);
-        }
-
-        if (args.length > 0) {
-            printer.print(args[0]);
-        } else {
-            printer.print("no params");
-        }
     }
+
 }
